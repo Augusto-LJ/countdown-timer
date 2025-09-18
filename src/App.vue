f89befec4b50a92499ffdf82130e72f612198015
Augusto-LJ <augustolimajardim@gmail.com>
2025-09-18 19:47:37 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100%;
+  background-image: url('@/assets/background-img.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+</style>
